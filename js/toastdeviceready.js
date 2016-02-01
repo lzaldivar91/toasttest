820610e72fa95ba885d82b75efbc290b2113bf78
unknown <lzaldivarlopez7329@2A304-D05.conestogac.on.ca>
2016-02-01 14:53:15 -0500

    hcdgfh

---
+++ js/toastdeviceready.js
@@ -0,0 +1,7 @@
+var phonegapReady = function(){
+    new jQuery.nd2Toast({message:"we are in phonegap"});
+};
+
+jQuery(document).ready(function(){
+    document.addEventListener("deviceready", phonegapReady, false);
+});
